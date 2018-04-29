2ca15d2a342ded94e1b2e6ab7dbb51990dfdeee8
Warren Green <s.green.warren@gmail.com>
2018-04-28 20:57:11 -0700

    make printing resize canvas generic

--- resize_canvas_for_printing.jsx
+++ resize_canvas_for_printing.jsx
@@ -1,4 +1,4 @@
-
+var printing_ratio = 8/10;
 var i;
 for (i=0;i<app.documents.length; i++){
 	app.activeDocument = app.documents[i];
@@ -6,10 +6,10 @@ for (i=0;i<app.documents.length; i++){
 	var nHeight = app.activeDocument.height;
 	if (app.activeDocument.width >= app.activeDocument.height) {
 		nWidth = app.activeDocument.width;
-		nHeight = nWidth * 0.8;
+		nHeight = nWidth * printing_ratio;
 	}else{
 		nHeight = app.activeDocument.height;
-		nWidth = nHeight * 0.8;
+		nWidth = nHeight * printing_ratio;
 	}
 	app.activeDocument.resizeCanvas(UnitValue(nWidth,"in"),UnitValue(nHeight,"in"),AnchorPosition.MIDDLECENTER);
 
